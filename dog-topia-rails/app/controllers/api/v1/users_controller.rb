f09db4b2e450ca7405828af8813af4405ac32423
Akash D <deshbhratarakash@gmail.com>
2024-05-10 16:10:29 +0530

    fixed: cleaned users controller

--- dog-topia-rails/app/controllers/api/v1/users_controller.rb
+++ dog-topia-rails/app/controllers/api/v1/users_controller.rb
@@ -2,16 +2,6 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authorize_request, except: :create
-      before_action :find_user, except: %i[create index]
-
-      def index
-        @users = User.all
-        render json: @users, status: :ok
-      end
-
-      def show
-        render json: @user, status: :ok
-      end
 
       def create
         @user = User.new(user_params)
@@ -19,36 +9,15 @@ module Api
         if @user.save
           render json: @user, status: :created
         else
-          render json: { errors: @user.errors.full_messages },
-                 status: :unprocessable_entity
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
-      end
-
-      def update
-        unless @user.update(user_params)
-          render json: { errors: @user.errors.full_messages },
-                 status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @user.destroy
       end
 
       private
 
-      def find_user
-        @user = User.find_by_username!(params[:_username])
-        rescue ActiveRecord::RecordNotFound
-          render json: { errors: 'User not found' }, status: :not_found
-      end
-
       def user_params
-        params.permit(
-          :first_name, :last_name, :email, :password, :password_confirmation
-        )
+        params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
-
     end
   end
 end
